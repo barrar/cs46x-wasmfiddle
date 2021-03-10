5b6f327a0dac937e4f2edaaf41b348175c09cdd4
barrar <barrar@users.noreply.github.com>
2021-03-10 10:47:44 +0000

    output to iframe with template, sample code w/ graphics

--- routes/compilerC.js
+++ routes/compilerC.js
@@ -7,22 +7,18 @@ path = require('path')
 
 router.post('/compilerC/receiver', async (req, res) => {
     let buildID = v4(); // Generate UUID
-
     let buildPath = path.join(__dirname, '../', 'builds/', buildID, '/');
     let sourcePath = path.join(buildPath, 'source.c');
 
     await exec("mkdir " + buildPath);
-
     await fs.writeFile(sourcePath, req.body.webassemblyEditorValue, { flag: 'w' }, (err) => {
         if (err) throw err;
     });
 
-    exec("/home/student/emsdk/upstream/emscripten/emcc ./source.c -o out.html &", { cwd: buildPath });
-
+    // when a warning is thrown the compiler fails to complete, so I used -Wno-everything
+    exec("/home/student/emsdk/upstream/emscripten/emcc -Wno-everything ./source.c -o out.html --shell-file ../../em_template.html &", { cwd: buildPath });
 
     res.status(200).json({ link: "https://wasmfiddle.ml/builds/" + buildID + "/out.html" });
-
-
 });
 
 module.exports = router;
